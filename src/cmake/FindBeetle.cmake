36c73f328a68112cbf8fc4e7842c2b366ca72a60
huluobo <hujewelz@163.com>
2020-06-17 17:50:24 +0800

    Update CmakeLists.txt

--- src/cmake/FindBeetle.cmake
+++ src/cmake/FindBeetle.cmake
@@ -1,8 +1,9 @@
+find_path(BEETLE_INCLUDE_DIR beetle.h 
+    ${CMAKE_INSTALL_PREFIX}/include/beetle 
+    /usr/include/beetle 
+    /usr/local/include/beetle)
 
-set(include_dir ${PROJECT_SOURCE_DIR}/src/Engine/product)
-
-find_path(BEETLE_INCLUDE_DIR beetle.h ${include_dir}/include/beetle /usr/include/beetle /usr/local/include/beetle)
-find_library(BEETLE_LIBRARY NAMES libbeetle.a PATHS ${include_dir}/lib /usr/lib /usr/local/lib)
+find_library(BEETLE_LIBRARY NAMES beetle PATHS ${CMAKE_INSTALL_PREFIX}/lib /usr/lib /usr/local/lib)
 
 message(STATUS "BEETLE_INCLUDE_DIR: ${BEETLE_INCLUDE_DIR}")
 message(STATUS "BEETLE_LIBRARY:  ${BEETLE_LIBRARY}")
